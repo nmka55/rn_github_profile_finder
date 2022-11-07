4ac958336d9b24b336ad7f6436116daed13be421
Nyambayar Amarsaikhan <nmka.8099@gmail.com>
2022-11-08 01:35:08 +0800

    initial commit, changes to boilerplate code

--- src/screens/home/tabA.js
+++ src/screens/home/tabA.js
@@ -1,26 +1,55 @@
-import {Button, View} from 'react-native';
+import { Button, ScrollView, View } from "react-native";
+import { FormProvider, useForm } from "react-hook-form";
+import {
+  RHFCheckbox,
+  RHFDatePicker,
+  RHFPicker,
+  RHFTextInput,
+} from "@app/components";
 
-import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {userLogout} from '@app/redux/actions';
+import React from "react";
+import { useSelector } from "react-redux";
 
-export default function TabA({navigation}) {
-  const userData = useSelector(state => state?.user?.userData ?? {});
-  const dispatch = useDispatch();
+export default function TabA({ navigation }) {
+  const userData = useSelector((state) => state?.user?.userData ?? {});
 
-  const {username = ''} = userData ?? {};
+  const form = useForm({
+    defaultValues: userData,
+  });
 
-  const onLogoutPress = () => {
-    dispatch(userLogout());
+  const onEndEditingUsername = (formValue) => {
+    console.log(formValue);
   };
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button
-        onPress={() => navigation.navigate('TabADetails')}
-        title={`Hello, ${username}. Go to User Details`}
-      />
-      <Button onPress={onLogoutPress} title={`Logout`} />
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: 16,
+        backgroundColor: "#FAFAFA",
+      }}
+    >
+      <FormProvider {...form}>
+        <RHFTextInput
+          name="searchQuery"
+          placeholder="GitHub username"
+          rules={{
+            required: "Enter GitHub username you want to search please!",
+          }}
+          returnKeyType="search"
+          onEndEditing={() => form.handleSubmit(onEndEditingUsername)()}
+        />
+      </FormProvider>
+
+      <ScrollView
+        style={{ flex: 1, marginTop: 16 }}
+        contentContainerStyle={{ flexGrow: 1, backgroundColor: "lightpink" }}
+      >
+        <View></View>
+      </ScrollView>
+      {/* <Button onPress={onLogoutPress} title={`Logout`} /> */}
     </View>
   );
 }
